f9711e16e91ece58ff8de5a5628ec61ff8c799c5
Naruedon-mu <marknarudon@gmail.com>
2025-11-14 21:43:31 +0700

    .

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ProductController = require("../controllers/product.controller");
 
-router.get("/", ProductController.getAllByFarm);
+router.get("/:FID", ProductController.getAllByFarm);
 router.get("/:PID", ProductController.getById);
 router.post("/", ProductController.create);
 router.put("/", ProductController.update);
