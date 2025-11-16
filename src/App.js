e0be53d48b18ea75a12bed7a1d7fde30db09d121
BOOKBIG <yanapatinkham@gmail.com>
2025-11-16 14:26:38 +0700

    Home ได้แล้ว งืออ

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import Login from './frontend/Auth/Login/';
 import Register from './frontend/Auth/Register/';
-import Home from './frontend/Home';           // เพิ่ม import Home
+import Home from './frontend/Home/Home';           // เพิ่ม import Home
 // import Navbar from './frontend/Navbar';  // เพิ่ม import Navbar
-import CreateFarm from './frontend/Farmfeatures/CreateFarm';
+import CreateFarm from './frontend/farmfeatures/CreateFarm';
 
 import { GlobalStyle } from "./GlobalStyle";
 import './App.css';
